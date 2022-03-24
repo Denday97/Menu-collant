35cedb9cbd2acd0d500e37fb121ed7e2b33e074d
Denday97 <eugenierony@gmail.com>
2022-03-24 13:17:45 +0100

    menu collant complet

--- fixed.js
+++ fixed.js
@@ -8,48 +8,73 @@ var scrollY = function() {
     return supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
 }
 
-
-var elements =document.querySelectorAll('[data-sticky]')
-for (var i = 0; i < elements.length; i++) {
-    (function(element) {
-        var rect = element.getBoundingClientRect()
+var makeSticky = function(element) {
+    var rect = element.getBoundingClientRect()
+        console.log(rect)
+        var offset = parseInt(element.getAttribute('data-offset') || 0, 10)
+        if (element.getAttribute('data-constraint')) {
+        var constraint = document.querySelector(element.getAttribute('data-constraint'))
+        } else {
+            var constraint = document.body
+        }
+        var constraintRect = constraint.getBoundingClientRect()
+        var constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
         var top = rect.top + scrollY()
-        var width = rect.width
         var fake = document.createElement('div')
         fake.style.width = rect.width + "px"
         fake.style.height = rect.height + "px"
 
         // fonctions
         var onScroll = function() {
-        var hasScrollClass = element.classList.contains('fixed')
-            if(scrollY() > top && !hasScrollClass) {
-                element.classList.add('fixed')
-                element.style.width = rect.width + "px"
-                element.parentNode.insertBefore(fake, element)
-            } else if(scrollY() < top && hasScrollClass) {
-                element.classList.remove('fixed')
-                element.parentNode.removeChild(fake)
+            if (scrollY() > constraintBottom && element.style.position != 'absolute') {
+                element.style.position ='absolute'
+                element.style.bottom = '0'
+                element.style.top ='auto'
+            } else if(scrollY() > top - offset && scrollY() < constraintBottom && element.style.position != 'fixed') {
+                    element.classList.add('fixed')
+                    element.style.position = 'fixed'
+                    element.style.top = offset + "px"
+                    element.style.bottom = 'auto'
+                    element.style.width = rect.width + "px"
+                    element.parentNode.insertBefore(fake, element)
+                } else if(scrollY() < top - offset && element.style.position != 'static') {
+                    element.classList.remove('fixed')
+                    element.style.position = 'static'
+                    if (element.parentNode.contains(fake)){
+                        element.parentNode.removeChild(fake)
+                    }
+                    
+                }
             }
-        }
 
-        var onResize = function () {
-            element.style.width = "auto"
-            element.classList.remove('fixed')
-            fake.style.display = "none"
-            rect = element.getBoundingClientRect()
-            top = rect.top + scrollY()
-            fake.style.width = rect.width + "px"
-            fake.style.height = rect.height + "px"
-            fake.style.display = "block"
-            onScroll()
-        }
-        window.addEventListener('scroll', onScroll)
+            var onResize = function () {
+                element.style.width = "auto"
+                element.classList.remove('fixed')
+                element.style.position ='static'
+                fake.style.display = "none"
+                rect = element.getBoundingClientRect()
+                constraintRect = constraint.getBoundingClientRect()
+                constraintBottom = constraintRect.top + scrollY() + constraintRect.height - offset - rect.height
+                top = rect.top + scrollY()
+                top = rect.top + scrollY()
+                fake.style.width = rect.width + "px"
+                fake.style.height = rect.height + "px"
+                fake.style.display = "block"
+                onScroll()
+            }
+            window.addEventListener('scroll', onScroll)
 
-        window.addEventListener('resize', onResize)
-
-        })(elements[i])
+            window.addEventListener('resize', onResize)
     
+    }
+
+
+var elements = document.querySelectorAll('[data-sticky]')
+for (var i = 0; i < elements.length; i++) {
+    makeSticky(elements[i])
 }
+              
+    
 
 
 })()
